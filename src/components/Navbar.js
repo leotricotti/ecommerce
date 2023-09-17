ac1d3de64ae1e14cd364dc07f9a40d8b2cd6699e
Leonardo Tricotti <tricottileo@gmail.com>
2023-09-17 09:18:01 -0300

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,76 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Stack from "@mui/material/Stack";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function appBarLabel(label) {
+function Navbar() {
   return (
-    <Toolbar>
-      <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-        {label}
-      </Typography>
-    </Toolbar>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <Link to="/">E-Store</Link>
+        <div
+          class="collapse navbar-collapse d-flex align-items-center mb-0"
+          id="navbarSupportedContent"
+        >
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 profile-menu d-flex align-items-center">
+            <div class="d-flex justify-content-center position-relative admin-panel">
+              <button
+                class="btn btn-link text-decoration-none admin-btn"
+                type="button"
+                onclick="goToAdminPanel()"
+              >
+                Admin Panel
+              </button>
+            </div>
+            <p class="text-white-50">Bienvenido</p>
+            <li class="nav-item dropdown">
+              <Link
+                class="nav-link dropdown-toggle"
+                to="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <span class="text-light me-3" id="'user-name"></span>
+                <i class="fas fa-user"></i>
+              </Link>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <Link class="dropdown-item" to="#">
+                    <i class="fas fa-sliders-h fa-fw"></i>
+                    Cuenta
+                  </Link>
+                </li>
+                <li>
+                  <Link class="dropdown-item" to="#">
+                    <i class="fas fa-cog fa-fw"></i>
+                    Configuración
+                  </Link>
+                </li>
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+                <li>
+                  <button class="btn dropdown-item" onclick="logout()">
+                    <i class="fas fa-sign-out-alt fa-fw"></i>
+                    Cerrar sesión
+                  </button>
+                </li>
+              </ul>
+            </li>
+            <button class="btn position-relative" onclick="setCartRoute()">
+              <Link to="" id="cart-route">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <span
+                  id="cart-badge"
+                  class="cart-basket d-flex align-items-center justify-content-center"
+                ></span>
+              </Link>
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#1976d2",
-    },
-  },
-});
-
-export default function EnableColorOnDarkAppBar() {
-  return (
-    <Stack spacing={2} sx={{ flexGrow: 1 }}>
-      <ThemeProvider theme={darkTheme}>
-        <AppBar position="static" color="primary">
-          {appBarLabel("E-Store")}
-        </AppBar>
-      </ThemeProvider>
-    </Stack>
-  );
-}
+export default Navbar;
